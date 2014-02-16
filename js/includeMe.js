d19dc1634f64a629aa393d463d01465183ad182d
Georgi Angelov <georgiangelov@Georgis-MacBook-Pro.local>
2014-02-16 00:40:35 -0600

    added a new js file to merge with current. Set size of table

---
+++ js/includeMe.js
@@ -0,0 +1,7 @@
+// $(function() {
+//   var image_width = $('#heart_img')[0].width();
+//   var image_height = $('#heart_img')[0].height();
+
+//   $('#pic_table').width(image_width);
+//   $('#pic_table').height(image_height);
+// });
